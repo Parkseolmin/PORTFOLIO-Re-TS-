8ea2bb57618f5cae7f011763087741ac082d6f7b
Parkseolmin <qkrtjfals2@naver.com>
2024-10-27 17:43:21 +0900

    data.ts update

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -43,7 +43,7 @@ export const data: Projects[] = [
     projectName: 'Momentum Web App',
     projectTech: 'React | React Query | useReducer | Context API',
     projectLink: 'https://momentum-wine.vercel.app/',
-    projectBlog: 'https://snowman-seolmini.tistory.com/101ㅋㄷ',
+    projectBlog: 'https://snowman-seolmini.tistory.com/101',
     projectGit: 'https://github.com/Parkseolmin/React_momentum',
     projectIntro:
       'Momentum은 Chrome App Store에 있는 Momentum을 본따 만든 사이트로, React 학습 초기 단계에서 제작되었습니다. 기본적인 상태 관리와 컴포넌트 구조를 익히기 위해 시작했으며, 이후 TanStack/React Query를 이용한 데이터 패칭 최적화, useReducer와 Context API를 통한 상태 관리를 도입하며 기능을 점진적으로 확장해 나갔습니다.',
@@ -130,8 +130,8 @@ export const data: Projects[] = [
     projectName: 'F.ROCK Web App',
     projectTech: 'React | Cloudinary | React Query | Firebase | Tailwind',
     projectLink: 'https://www.naver.com/',
-    projectBlog: '#',
-    projectGit: '#',
+    projectBlog: 'https://snowman-seolmini.tistory.com/85',
+    projectGit: 'https://github.com/Parkseolmin/React_R.FROCK',
     projectIntro:
       '이것은 쇼핑몰Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint recusandae optio quae cumque vel temporibus possimus deleniti magni ratione illo expedita tenetur, ullam nesciunt odit ut saepe ipsa odio ipsum!',
     introVideo: '/img/frock/frock.mp4',
